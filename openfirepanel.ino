20c232ad97a7c52ad347d26b5e3b7122e1649303
jelimoore <jelimoore@gmail.com>
2017-11-29 12:15:02 -0600

    last commit from web i promise

--- openfirepanel.ino
+++ openfirepanel.ino
@@ -1,10 +1,33 @@
+/*
+ * OpenFirePanel
+ * 
+ * Written by Jack Moore
+ * 
+ * How to use:
+ * 1. Modify settings.h with your options
+ * 2. Compile and upload
+ * 
+ * REVISIONS
+ * 11-28-17: Starting to play with some basic code
+ * 
+ */
+
 #include "settings.h"
-#include <Password.h>
-#include <LiquidCrystal.h>
+
+String sysver = "0.1a";
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(20,4);
+  lcd.print("OpenFirePanel");
+  lcd.setCursor(0,1);
+  lcd.print("Ver: ");
+  lcd.setCursor(5,1);
+  lcd.print(sysver);
+  lcd.setCursor(0,2);
+  lcd.print("Booting...");
+  delay(1000);
 
+  pinMode(
 }
 
 void loop() {
